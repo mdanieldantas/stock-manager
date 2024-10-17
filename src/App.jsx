4ea40691e925a4c98d494567f79dc2196821d6c8
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-17 18:03:49 -0300

    feat: Adicionar contexto de estoque para gerenciar os itens
    Adiciona o contexto de estoque para gerenciar os itens do estoque. O contexto é utilizado para armazenar e manipular os dados dos itens, como adicionar um novo item ao estoque. Isso é feito através da criação do contexto StockContext e do provedor StockContextProvider, que envolvem a aplicação principal. Além disso, o provedor utiliza o localStorage para armazenar os itens do estoque de forma persistente.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router"
+import { StockContextProvider } from "./contexts/StockContext"
+
 
 function App() {
 
   return (
+  <StockContextProvider>
     <RouterProvider router={router} />
+  </StockContextProvider>
   )
 }
 
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,33 @@
+// Importamos o Link, Outlet e useLocation do react-router-dom
 import { Link, Outlet, useLocation } from "react-router-dom";
 
+// Função principal do layout de itens
 export default function ItemsLayout() {
-  const { pathname } = useLocation()
+  // useLocation é usado para obter o caminho atual da URL e o pathname para obter apenas o caminho da URL atual vai ser usado mais abaixo para estilizar a aba ativa
+  const { pathname } = useLocation();
 
   return (
     <main>
+      {/* Título principal da página */}
       <h1>Stock Items</h1>
+      
+      {/* Div para os links de navegação em forma de abas */}
       <div className="tabs">
-        <Link to="/items" className={`tab ${pathname === "/items" ? "active" : ""}`}>Todos os itens</Link>
-        <Link to="/items/new" className={`tab ${pathname === "/items/new" ? "active" : ""}`}>Novo Item</Link>
+        {/* Link para todos os itens 
+        Classe Dinâmica: A classe "active" é adicionada ao link quando o pathname atual é "/items". Isso é útil para estilizar a aba ativa, indicando visualmente ao usuário em qual seção ele está.
+         */}
+        <Link to="/items" className={`tab ${pathname === "/items" ? "active" : ""}`}>
+          Todos os itens
+        </Link>
+        
+        {/* Link para a criação de um novo item */}
+        <Link to="/items/new" className={`tab ${pathname === "/items/new" ? "active" : ""}`}>
+          Novo Item
+        </Link>
       </div>
+      
+      {/* Outlet é onde os componentes filhos serão renderizados */}
       <Outlet />
     </main>
-  )
+  );
 }
