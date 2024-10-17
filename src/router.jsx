372a8c473a34bae86ffb752242ae676964e53101
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-17 14:30:50 -0300

    feat: Adicionar roteador de navegação e páginas
    Adiciona o roteador de navegação e as páginas Home, ItemsLayout, ListItems, CreateItem, UpdateItem e ShowItem ao projeto.

--- src/router.jsx
+++ src/router.jsx
@@ -1,3 +1,33 @@
 import { createBrowserRouter  } from "react-router-dom";
+import Home from "./pages/Home";
+import ItemsLayout from "./pages/ItemsLayout";
+import ListItems from "./pages/ListItems";
+import CreateItem from "./pages/CreateItem";
+import UpdateItem from "./pages/UpdateItem";
+import ShowItem from "./pages/ShowItem";
+import RootLayout from "./pages/RootLayout";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children:[
+            {
+                index: true,
+                element:<Home/>
+            },
+            {
+                path:"items",
+                element:<ItemsLayout/>,
+                children:[
+                    {index:true,element:<ListItems/>},
+                    {path:"new",element:<CreateItem />},
+                    {path:":id",element:<ShowItem/>},
+                    {path:":id/update",element:<UpdateItem />},
+                ]
+            }
+        ]
+    }
+])
+
+export default router;
