4232e6133edf2577d575f8dd81fc7e6d16de9a99
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-18 12:28:42 -0300

    Refatorar importações de componentes em ListItems, ShowItem e UpdateItem

--- src/contexts/StockContext.jsx
+++ src/contexts/StockContext.jsx
@@ -1,45 +1,65 @@
 import { createContext, useState } from "react";
-import PropTypes from "prop-types"
+import PropTypes, { object } from "prop-types";
 
-export const StockContext = createContext({})
+export const StockContext = createContext({});
 
 StockContextProvider.propTypes = {
-  children: PropTypes.node
-}
-// item 
+  children: PropTypes.node,
+};
+// item
 // {name, description, quantity, price, category, createdAt, updatedAt}
 
-export function StockContextProvider({children}){ 
-    const [items, setItems] = useState (()=>{
-        const storedItems = localStorage.getItem('obc-react-stock')
-        if (!storedItems) return []
-        const items = JSON.parse(storedItems)
-        items.forEach((item)=>{
-            item.createdAt= new Date(item.createdAt)
-            item.updatedAt= new Date(item.updatedAt)
-        })
-        return items
-    })
-     
-    const addItem = (item) => {
-        setItems(currentState => {
-            const updateItems = [item, ...currentState]
-            localStorage.setItem('obc-react-stock', JSON.stringify(updateItems))
-            return updateItems
-        })
-    }
-     
-    const stock ={
-        items,
-        addItem
-    }
+export function StockContextProvider({ children }) {
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem("obc-react-stock");
+    if (!storedItems) return [];
+    const items = JSON.parse(storedItems);
+    items.forEach((item) => {
+      item.createdAt = new Date(item.createdAt);
+      item.updatedAt = new Date(item.updatedAt);
+    });
+    return items;
+  });
 
+  const addItem = (item) => {
+    setItems((currentState) => {
+      const updateItems = [item, ...currentState];
+      localStorage.setItem("obc-react-stock", JSON.stringify(updateItems));
+      return updateItems;
+    });
+  };
 
-    return(
-      <StockContext.Provider value={stock}>
-      {children}
-      </StockContext.Provider>
-       
-    )
+  const getItem = (itemId) => {
+    return items.find((item) => item.id === +itemId);
+  };
+
+  const updateItem = (itemId, newAttributes) => {
+      setItems(currentState => {
+          const itemIndex = currentState.findIndex(item => item.id === +itemId)
+          const updateItems = [...currentState]
+          object.assingn(updateItems[itemIndex], newAttributes, {updatedAt:new Date()})
+          localStorage.setItem("obc-react-stock", JSON.stringify(updateItems))
+          return updateItems
+      })
+  }
+
+  const deleteItem = (itemId) => {
+    setItems((currentState) => {
+      const updateItems = currentState.filter((item) => item.id !== itemId);
+      localStorage.setItem("obc-react-stock", JSON.stringify(updateItems));
+      return updateItems;
+    });
+  };
+
+  const stock = {
+    items,
+    addItem,
+    deleteItem,
+    getItem,
+    updateItem
+  };
+
+  return (
+    <StockContext.Provider value={stock}>{children}</StockContext.Provider>
+  );
 }
-
